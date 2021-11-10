0e9f98b5264562883aebbb88f0aa60f0e218f1ae
SheeshpalSinghSAini <123sheeshpalsinghsaini@gmail.com>
2021-11-10 12:56:15 +0530

    FrontEndNotes

---
+++ JavaScript/JavaScript/script.js
@@ -0,0 +1,26 @@
+// // console.log("Hello World, this msg is appearing");
+// // alert("Hello World");
+
+
+// document.getElementById("h1").innerHTML="Sheeshpal Singh Saini";
+
+// document.write("123sheeshpalsinghsaini@gmail.com");
+
+console.log("HelloWorld");
+
+/*
+variable in javascript
+var x;//old way
+let x;//new way
+const x=9;//constant variable
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ JavaScript/notes.js
@@ -0,0 +1,159 @@
+/*
+Note: In one web page it may be more than one javascript file
+
+
+
+
+------------------------------  Manual Refference ------------------------
+(MDN)
+
+================================Why JavaScript Born===================
+1. To creatre interactive websites.
+    - Client side Validation
+    -Popup
+    -Event on click
+    -And many more......
+
+
+
+
+--------------Where is JavaScript now?----------------
+* Website Client Side(JS,Jqery,React js .....)
+* Website Server Side(Node JS, Express js)
+* Mobile Developement(React Native, Phone Gap, ionic......)
+* Software Developement(electronic EX-VScode)
+
+
+
+
+
+
+
+-----------------What is ECMAScript?-----------------------------------
+There is an organization "ecma"
+* Ecma International
+* An organization that creates standards(Rules) for technologies.
+Ecma relise a standard:
+    ECMA-262:
+        -This is a standard published by Ecma International. It contains the 
+        specification for a general purpose scripting language.
+
+        -https://www.ecma-international.org/publications/standards/Ecma-262.html
+    ECMAScript:
+        - The specification defined in ECMA-262 for creating a general purpose 
+        scritping language.
+
+        - ECMAScript provides the rules, details, and guidelines that a scripting 
+        language must observe to be considered ECMAScript compliant.
+
+    JavaScript:
+        - A general purpose scritping language that conforms to the ECMAScript
+        specification.[JS developed by use ECMAScript]
+
+        
+        -By reading the ECMAScript specification, you learn how to create a script
+        language.
+        -By reading the JavaScript documentation, you learn how to use a scripting 
+        language.
+    
+
+        JavaScript implements the ECMAScript specification as described in ECMA-262.
+
+
+
+
+
+
+-----------------What is ES6 ES7, ES8 ........AND SO ON...?---------------  
+* ES-6 ------> ECMAScript 6
+* It is the sixth edition of the ECMA-262 standard, and features major changes 
+and improvements to the ECMAScript specification.
+* Synonyms: ES6, ES2015, and ECMAScript 2015
+*ES6 and ES2015 are two different names for the same thing.
+
+
+
+
+------------------------------------------------------------------------
+* ES1 1997                                                              |
+* ES2 1998                                                              |
+* ES3 1999                                                              |
+* ES4 Abandoned                                                         |
+* ES5 2009                                                              |
+* ES6 2015 or ECMAScript 2015(ES2015)-6th edition (Major change)        |
+* ES7 2016 or ECMAScript 2016(ES2016)-7th edition                       |
+* ES8 2017 or ECMAScript 2017(ES2017)-8th edition                       |
+* ES9 2018 or ECMAScript 2018(ES2018)-9th edition                       |
+* ES1- 2019 or ECMAScript 2019(ES2019)-10th edition                     |
+* ES11 2020 or ECMAScript 2020(ES2020)-11th edition                     |
+-------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------------
+Compatibility Issue[Firefox, Google Chrome, Internet Explorer, Safari, Opera]      |
+JavaScript Engine: V8 in Chrome, SpiderMonkey in Firefox and Chakra in Edge        |
+----------------------------------------------------------------------------------
+htps://caniuse.com  -------website for check that element in which version are supported or not
+
+
+---------------------------------------------------------------------------------------------------
+                            Start JavaScript
+---------------------------------------------------------------------------------------------------
+------------------------------------------------
+There are three way to write JavaScript Code    |
+1.  In .js file                                 |
+2.  In HTML file[use script tag]                |
+3.  In console of chrome(browser)               |
+-----------------------------------------------
+
+Note:- for use new line in chrome enter with shift key.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
